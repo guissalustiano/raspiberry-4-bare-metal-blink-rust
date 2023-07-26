b66e903cfb35d6359e58ea0a923f2c38bd16325b
Guilherme Salustiano <guissalustiano@gmail.com>
2023-07-26 20:06:12 -0300

    run only with core 0

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use aarch64_cpu::asm::nop;
 
 use panic_halt as _;
+use tock_registers::interfaces::Readable;
 
 // https://datasheets.raspberrypi.com/bcm2711/bcm2711-peripherals.pdf
 const START:            usize = 0xFE00_0000; // Based on section 1.2 of manual
@@ -14,6 +15,10 @@ const GPIO_START:       usize = START + GPIO_OFFSET;
 #[no_mangle]
 #[link_section = ".text._start"]
 pub extern "C" fn _start() -> ! {
+    if get_cpu_id() != 0 {
+        aarch64_cpu::asm::wfe();
+    }
+
     unsafe {
         // Set GPIO 42 with GPFSEL4[8:6] = 0b001
         core::ptr::write_volatile((GPIO_START + 0x10) as *mut u32, 0b001 << 6);
@@ -35,4 +40,8 @@ pub extern "C" fn _start() -> ! {
             }
         }
     }
+}
+
+fn get_cpu_id() -> u64 {
+    aarch64_cpu::registers::MPIDR_EL1.get() & 0b11
 }
